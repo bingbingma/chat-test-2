b60bcfef4be3ab8d7c86e9bdcbe991d439a5f016
bingbingma <7047982+bingbingma@users.noreply.github.com>
2019-04-27 12:00:43 -0400

    working locally

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ let express = require("express");
 let app = express();
 let path = require("path");
 let server = require("http").createServer(app);
-let io = require("../..")(server);
+let io = require("socket.io")(server);
 let port = process.env.PORT || 3000;
 
 server.listen(port, () => {
-  console.log("Server listening at port %d", port);
+  console.log("Server listening at port http://localhost:" + port);
 });
 
 // Routing
